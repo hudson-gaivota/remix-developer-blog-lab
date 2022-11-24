0e966a0073fb7e943518574e98b1db36440fa49f
Hudson Pereira <hudson.pereira@gaivota.ai>
2022-11-24 02:42:40 -0300

    update feature

---
+++ app/routes/posts/admin/$slug.tsx
@@ -0,0 +1,128 @@
+import {
+  ActionFunction,
+  json,
+  LoaderFunction,
+  redirect,
+} from "@remix-run/node";
+import {
+  Form,
+  useActionData,
+  useLoaderData,
+  useTransition,
+} from "@remix-run/react";
+import { useRef, useState } from "react";
+import invariant from "tiny-invariant";
+import { createPost, getPost, Post, updatePost } from "~/models/post.server";
+
+type LoaderData = { post: Post };
+
+type ActionData =
+  | {
+      title: null | string;
+      slug: null | string;
+      markdown: null | string;
+    }
+  | undefined;
+
+export const loader: LoaderFunction = async ({ params }) => {
+  console.log("pls");
+  invariant(params.slug, `params.slug is required`);
+  const post = await getPost(params.slug);
+  // console.log(post);
+  invariant(post, `Post not found: ${params.slug}`);
+  return json<LoaderData>({ post });
+};
+
+export const action: ActionFunction = async ({ request }) => {
+  const formData = await request.formData();
+
+  const title = formData.get("title");
+  const slug = formData.get("slug");
+  const markdown = formData.get("markdown");
+
+  const errors: ActionData = {
+    title: title ? null : "Title is required",
+    slug: slug ? null : "Slug is required",
+    markdown: markdown ? null : "Markdown is required",
+  };
+  const hasErrors = Object.values(errors).some((errorMessage) => errorMessage);
+  if (hasErrors) {
+    return json<ActionData>(errors);
+  }
+
+  invariant(typeof title === "string", "title must be a string");
+  invariant(typeof slug === "string", "slug must be a string");
+  invariant(typeof markdown === "string", "markdown must be a string");
+
+  await updatePost({ title, slug, markdown });
+
+  return redirect("/posts/admin");
+};
+
+const inputClassName = `w-full rounded border border-gray-500 px-2 py-1 text-lg read-only:bg-gray-200 read-only:cursor-not-allowed`;
+
+export default function AdminPost() {
+  const { post } = useLoaderData();
+  const errors = useActionData();
+  const transition = useTransition();
+  const isUpdating = Boolean(transition.submission);
+
+  return (
+    <Form method="post" key={post.slug}>
+      <p>
+        <label>
+          Post Title:{" "}
+          {errors?.title ? (
+            <em className="text-red-600">{errors.title}</em>
+          ) : null}
+          <input
+            defaultValue={post.title}
+            type="text"
+            name="title"
+            className={inputClassName}
+          />
+        </label>
+      </p>
+      <p>
+        <label>
+          Post Slug:{" "}
+          {errors?.slug ? (
+            <em className="text-red-600">{errors.slug}</em>
+          ) : null}
+          <input
+            type="text"
+            name="slug"
+            className={inputClassName}
+            value={post.slug}
+            readOnly
+          />
+        </label>
+      </p>
+      <p>
+        <label htmlFor="markdown">
+          Markdown:{" "}
+          {errors?.markdown ? (
+            <em className="text-red-600">{errors.markdown}</em>
+          ) : null}
+        </label>
+        <br />
+        <textarea
+          id="markdown"
+          rows={20}
+          name="markdown"
+          className={`${inputClassName} font-mono`}
+          defaultValue={post.markdown}
+        />
+      </p>
+      <p className="text-right">
+        <button
+          type="submit"
+          className="rounded bg-blue-500 py-2 px-4 text-white hover:bg-blue-600 focus:bg-blue-400 disabled:bg-blue-300"
+          disabled={isUpdating}
+        >
+          {isUpdating ? "Updating..." : "Update Post"}
+        </button>
+      </p>
+    </Form>
+  );
+}
